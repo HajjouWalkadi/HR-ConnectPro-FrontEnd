2d02c5b38c68138fe6b3a447c39bf978ca043265
hajjou walkadi <walkadihajjou153@gmail.com>
2024-03-20 11:56:43 +0000

    get and save department was added successfully

---
+++ src/app/feature-module/employees/departments/departments.component.ts
@@ -0,0 +1,222 @@
+// departments.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { DepartmentService } from 'src/app/core/services/department.service';
+import { Department } from 'src/app/models/department';
+import { DataService, apiResultFormat, getDepartment, routes } from 'src/app/core/core.index';
+import { pageSelection } from '../designations/designations.component';
+
+
+@Component({
+  selector: 'app-departments',
+  templateUrl: './departments.component.html',
+  styleUrls: ['./departments.component.scss']
+})
+export class DepartmentsComponent implements OnInit {
+  departments: Department[] = [];
+  public routes = routes;
+  newDepartment: Department = {id: 0, nom: ''};
+
+  //pagination variables
+  public lastIndex = 0;
+  public pageSize = 10;
+  public totalData = 0;
+  public skip = 0;
+  public limit: number = this.pageSize;
+  public pageIndex = 0;
+  public serialNumberArray: Array<number> = [];
+  public currentPage = 1;
+  public pageNumberArray: Array<number> = [];
+  public pageSelection: Array<pageSelection> = [];
+  public totalPages = 0;
+  //** / pagination variables
+
+    public changePageSize(): void {
+    this.pageSelection = [];
+    this.limit = this.pageSize;
+    this.skip = 0;
+    this.currentPage = 1;
+    // this.getTableData();
+  }
+
+ 
+
+  constructor(private departmentService: DepartmentService) {}
+
+  ngOnInit(): void {    
+    this.loadDepartments();
+    // this.getTableData();
+  }
+
+  loadDepartments() {
+    this.departmentService.getAllDepartments().subscribe(departments => {
+      this.departments = departments;
+      console.log(departments);
+      
+    });
+  }
+
+  addDepartment() {
+    this.departmentService.addDepartment(this.newDepartment).subscribe(
+      () => {
+        this.loadDepartments();
+        this.newDepartment = {id: 0, nom: ''};
+        // this.modalService.dismiss('add_department'); // Close the modal using NgbModal
+      },
+      error => {
+        console.error('Error adding department:', error);
+        // Handle any errors here
+      }
+    );
+  }
+
+
+  // Add methods to handle adding, updating, deleting departments
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { Sort } from '@angular/material/sort';
+// import { MatTableDataSource } from '@angular/material/table';
+// import { DataService, apiResultFormat, getDepartment, routes } from 'src/app/core/core.index';
+
+// @Component({
+//   selector: 'app-departments',
+//   templateUrl: './departments.component.html',
+//   styleUrls: ['./departments.component.scss']
+// })
+// export class DepartmentsComponent implements OnInit {
+//   public lstDepartment: Array<getDepartment> = [];
+//   public searchDataValue = '';
+//   dataSource!: MatTableDataSource<getDepartment>;
+//   public routes = routes;
+//   // pagination variables
+//   public lastIndex = 0;
+//   public pageSize = 10;
+//   public totalData = 0;
+//   public skip = 0;
+//   public limit: number = this.pageSize;
+//   public pageIndex = 0;
+//   public serialNumberArray: Array<number> = [];
+//   public currentPage = 1;
+//   public pageNumberArray: Array<number> = [];
+//   public pageSelection: Array<pageSelection> = [];
+//   public totalPages = 0;
+//   //** / pagination variables
+
+//   constructor(private data: DataService) {}
+
+//   ngOnInit(): void {
+//     this.getTableData();
+//   }
+
+//   private getTableData(): void {
+//     this.lstDepartment = [];
+//     this.serialNumberArray = [];
+
+//     this.data.getDepartment().subscribe((res: apiResultFormat) => {
+//       this.totalData = res.totalData;
+//       res.data.map((res: getDepartment, index: number) => {
+//         const serialNumber = index + 1;
+//         if (index >= this.skip && serialNumber <= this.limit) {
+//           res.id = serialNumber;
+//           this.lstDepartment.push(res);
+//           this.serialNumberArray.push(serialNumber);
+//         }
+//       });
+//       this.dataSource = new MatTableDataSource<getDepartment>(this.lstDepartment);
+//       this.calculateTotalPages(this.totalData, this.pageSize);
+//     });
+
+ 
+//   }
+
+//   public sortData(sort: Sort) {
+//     const data = this.lstDepartment.slice();
+
+//     /* eslint-disable @typescript-eslint/no-explicit-any */
+//     if (!sort.active || sort.direction === '') {
+//       this.lstDepartment = data;
+//     } else {
+//       this.lstDepartment = data.sort((a: any, b: any) => {
+//         const aValue = (a as any)[sort.active];
+//         const bValue = (b as any)[sort.active];
+//         return (aValue < bValue ? -1 : 1) * (sort.direction === 'asc' ? 1 : -1);
+//       });
+//     }
+//   }
+
+//   public searchData(value: string): void {
+//     this.dataSource.filter = value.trim().toLowerCase();
+//     this.lstDepartment = this.dataSource.filteredData;
+//   }
+
+//   public getMoreData(event: string): void {
+//     if (event === 'next') {
+//       this.currentPage++;
+//       this.pageIndex = this.currentPage - 1;
+//       this.limit += this.pageSize;
+//       this.skip = this.pageSize * this.pageIndex;
+//       this.getTableData();
+//     } else if (event === 'previous') {
+//       this.currentPage--;
+//       this.pageIndex = this.currentPage - 1;
+//       this.limit -= this.pageSize;
+//       this.skip = this.pageSize * this.pageIndex;
+//       this.getTableData();
+//     }
+//   }
+
+//   public moveToPage(pageNumber: number): void {
+//     this.currentPage = pageNumber;
+//     this.skip = this.pageSelection[pageNumber - 1].skip;
+//     this.limit = this.pageSelection[pageNumber - 1].limit;
+//     if (pageNumber > this.currentPage) {
+//       this.pageIndex = pageNumber - 1;
+//     } else if (pageNumber < this.currentPage) {
+//       this.pageIndex = pageNumber + 1;
+//     }
+//     this.getTableData();
+//   }
+
+//   public changePageSize(): void {
+//     this.pageSelection = [];
+//     this.limit = this.pageSize;
+//     this.skip = 0;
+//     this.currentPage = 1;
+//     this.getTableData();
+//   }
+
+//   private calculateTotalPages(totalData: number, pageSize: number): void {
+//     this.pageNumberArray = [];
+//     this.totalPages = totalData / pageSize;
+//     if (this.totalPages % 1 !== 0) {
+//       this.totalPages = Math.trunc(this.totalPages + 1);
+//     }
+//     for (let i = 1; i <= this.totalPages; i++) {
+//       const limit = pageSize * i;
+//       const skip = limit - pageSize;
+//       this.pageNumberArray.push(i);
+//       this.pageSelection.push({ skip: skip, limit: limit });
+//     }
+//   }
+// }
+// export interface pageSelection {
+//   skip: number;
+//   limit: number;
+// }
