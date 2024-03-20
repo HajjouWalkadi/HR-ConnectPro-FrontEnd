9b7de835d20b7b8dc4e1b12eb0dab2e0e4b264c8
hajjou walkadi <walkadihajjou153@gmail.com>
2024-03-20 11:57:05 +0000

    service department was added

---
+++ src/app/core/services/department.service.ts
@@ -0,0 +1,24 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Department } from 'src/app/models/department';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+  private baseUrl = 'http://localhost:8080/api/v1/departement';
+
+  constructor(private http: HttpClient) {}
+
+  getAllDepartments(): Observable<Department[]> {
+    return this.http.get<Department[]>(`${this.baseUrl}`).pipe(map((result: any) => result.data));
+  }
+
+  addDepartment(department: Department): Observable<Department> {
+    return this.http.post<Department>(`${this.baseUrl}/add`, department);
+  }
+
+  // Add other methods for updating, deleting, etc.
+}
