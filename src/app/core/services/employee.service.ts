a55cdb871f336544737fc7121da21437061dd96e
hajjou walkadi <walkadihajjou153@gmail.com>
2024-03-21 11:51:09 +0000

    addEmlpoyee was added in employee.service.ts

--- src/app/core/services/employee.service.ts
+++ src/app/core/services/employee.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, map } from "rxjs";
 import { User } from "src/app/modal/entities/user.interface";
+import { Employee } from "src/app/models/employee";
 
 @Injectable({ providedIn: 'root'})
 export class employeeService {
@@ -12,5 +13,9 @@ export class employeeService {
 
     findAll() : Observable<User[]> {
         return this.http.get<any>(this.baseUrl).pipe(map(data=> data.data));
+    }
+
+    addEmployee(employee: Employee): Observable<any> {
+        return this.http.post<Employee>(`${this.baseUrl}/save`, employee);
     }
 }
